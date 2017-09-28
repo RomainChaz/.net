9389b6698a2de626e77f5522c226fe43c714b8c3
Rominou <romainchazottier@gmail.com>
2017-09-28 13:54:18 +0200

    first commit

---
+++ WSConvertisseur/Controllers/DeviseController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WSConvertisseur.Models;
+using System.Web.Http.Description;
+
+namespace WSConvertisseur.Controllers
+{
+    public class DeviseController : ApiController
+    {
+        List<Devise> devises = new List<Devise>();
+
+        public DeviseController()
+        {
+            devises.Add(new Devise(1,"Dollar", 1.08));
+            devises.Add(new Devise(2, "Franc Suisse", 1.07));
+            devises.Add(new Devise(3, "Yen", 120));
+
+        }
+
+        // GET: api/Devise
+        public IEnumerable<Devise> Get()
+        {
+            return devises.AsEnumerable();
+        }
+
+        // GET: api/Devise/5
+        [ResponseType(typeof(Devise))]
+        public IHttpActionResult Get(int id)
+        {
+            Devise devise = devises.FirstOrDefault((d) => d.Id == id);
+            if (devise == null)
+            {
+                return NotFound();
+            }
+            return Ok(devise);
+        }
+
+        // POST: api/Devise
+        [ResponseType(typeof(Devise))]
+        public IHttpActionResult Post(Devise devise)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            devises.Add(devise);
+            return CreatedAtRoute("DefaultApi", new { id = devise.Id }, devise);
+        }
+
+        // PUT: api/Devise/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult Put(int id, Devise devise)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            if (id != devise.Id)
+            {
+                return BadRequest();
+            }
+
+            int index = devises.FindIndex((d) => d.Id == id);
+            if (index < 0)
+            {
+                return NotFound();
+            }
+            devises[index] = devise;
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // DELETE: api/Devise/5
+        [ResponseType(typeof(Devise))]
+        public IHttpActionResult Delete(int id)
+        {
+            Devise devise = devises.FirstOrDefault((d) => d.Id == id);
+            if (devise == null)
+            {
+                return NotFound();
+            }
+            devises.Remove(devise);
+            return Ok(devise);
+        }
+    }
+}
---
+++ WSConvertisseur/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Les informations générales relatives à un assembly dépendent de 
+// l’ensemble d’attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
+// associées à un assembly.
+[assembly: AssemblyTitle("WSConvertisseur")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WSConvertisseur")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// L’affectation de la valeur false à ComVisible rend les types invisibles dans cet assembly
+// aux composants COM. Si vous devez accéder à un type dans cet assembly à partir de
+// COM, affectez la valeur true à l’attribut ComVisible sur ce type.
+[assembly: ComVisible(false)]
+
+// Le GUID suivant est pour l’ID de la typelib si ce projet est exposé à COM
+[assembly: Guid("83a48e39-8c3d-4166-a35e-4247d10a75c9")]
+
+// Les informations de version pour un assembly se composent des quatre valeurs suivantes :
+//
+//      Version principale
+//      Version mineure
+//      Numéro de build
+//      Révision
+//
+// Vous pouvez spécifier toutes les valeurs ou vous pouvez définir par défaut les valeurs Révision et Numéro de version
+// en utilisant ’*’, comme indiqué ci-dessous :
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
